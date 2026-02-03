a1826bfb8ab92083b23be9d71a6e7e043a49b3fe
sanulisiya <Sanulisiyathma93@gmail.com>
2026-02-03 15:15:46 +0530

    Docs: Data Prerocsing and cleaning  in developedreasearch model

--- health-risk-recommendations/main.py
+++ health-risk-recommendations/main.py
@@ -1,6 +1,16 @@
-def main():
-    print("Hello from health-risk-recommendations!")
+import pandas as pd
+import numpy as np
 
+# Visualization (optional but pro)
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-if __name__ == "__main__":
-    main()
+# ML
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+
+from xgboost import XGBClassifier
+
+# Save model
+import joblib
