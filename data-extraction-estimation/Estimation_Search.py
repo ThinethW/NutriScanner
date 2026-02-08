d0384e4ecbb994f5956058174bbf0cc4cc470b00
Thineth Weerasinghe <91240232+cuboidalstudios@users.noreply.github.com>
2026-02-08 23:07:10 +0530

    readme, connected classes

---
+++ data-extraction-estimation/Estimation_Search.py
@@ -0,0 +1,22 @@
+def Estimation_Search(data):
+    values = {"calories": -1,
+              "proteins": -1,
+              "fats": -1,
+              "carbohydrates": -1,
+              "sodium": -1,
+              "Magnesium": -1,
+              "calcium": -1,
+              "iron": -1,
+              "zinc": -1,
+              "vitamin A": -1,
+              "vitamin B": -1,
+              "vitamin C": -1,
+              "vitamin D": -1,
+              "vitamin E": -1,
+              "vitamin K": -1,
+              "vitamin B1": -1,
+              "vitamin B2": -1,
+              "vitamin B3": -1,
+              "vitamin B6": -1,
+              "vitamin B12": -1, }
+    return values
