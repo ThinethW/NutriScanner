57a2c29eb9b9219c4a522a708e5246e3c4ea87a1
Thineth Weerasinghe <91240232+cuboidalstudios@users.noreply.github.com>
2026-02-09 02:17:17 +0530

    Update on Estimation_Search.py and related

--- data-extraction-estimation/Est_Handler.py
+++ data-extraction-estimation/Est_Handler.py
@@ -4,6 +4,14 @@ import Logger
 
 
 class Est_Handler:
+    """
+    this is the general handler for this module.
+
+    data contains 2 possible input types that define whether or not it is a label or an image:
+    string: label
+    list: image (the list contains tuples consisting of the item name and quantity... (name, quantity))
+    """
+
     def __init__(self):
         self.data = None
         self.Logger = Logger.Logger("data-extraction-estimation")
@@ -27,11 +35,15 @@ class Est_Handler:
                 values = Label_Extraction.Label_Extraction(self.data)
                 self.Logger.log(f"INFO: values returned \n{values}")
 
-            elif type(self.data) == tuple:
-                self.Logger.log("INFO: Data selected: Image")
-                values = Estimation_Search.Estimation_Search(self.data)
-                self.Logger.log(f"INFO: values returned \n{values}")
+            elif type(self.data) == list:
+                for i in self.data:
+                    if type(i) == tuple:
+                        self.Logger.log(f"INFO: Data selected: Image\t\tselected: {i}")
+                        values = Estimation_Search.Estimation_Search(self.data, logger=self.Logger)
+                        self.Logger.log(f"INFO: values returned \n{values}")
+                    else:
+                        self.Logger.log(f"ERROR: Not expected Datatype: List did not contain tuple. contained {type(i)}")
             else:
-                self.Logger.log("ERROR: Not expected Datatype")
+                self.Logger.log("ERROR: Not expected Datatype. Layer 1")
                 values = None
             return values
